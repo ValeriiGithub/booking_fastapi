f81d7b7ae94bd77755930f266c2e5cdb2662b4d0
Valerii <valeri.github@gmail.com>
2023-11-26 20:38:34 +0300

    create app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
-@app.get("/hotels")
-def get_hotels():
-    return "Отель Бридж Резорт 5 звезд"
+@app.get("/hotels/{hotel_id}")
+def get_hotels(hotel_id: int, date_from, date_to):
+    # return "Отель Бридж Резорт 5 звезд"
+    return hotel_id, date_from, date_to
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
