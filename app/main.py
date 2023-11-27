403658ff6d19b5cac61e80b8383e2634809fe748
Valerii <valeri.github@gmail.com>
2023-11-27 18:07:22 +0300

    add validation

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from  typing import Optional
+from datetime import date
 
 app = FastAPI()
 
 @app.get("/hotels")
 def get_hotels(
-        location,
-        date_from,
-        date_to,
+        location: str,
+        date_from: date,
+        date_to: date,
         has_spa: Optional[bool] = None,
-        stars: Optional[int] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
 
 ):
 
