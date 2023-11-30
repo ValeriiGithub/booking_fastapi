0a5109effc4049c0d02c397dcc72e43dce3e969d
Valerii <valeri.github@gmail.com>
2023-11-30 20:50:58 +0300

    Создать класс с аргументами для get запросов

--- app/main.py
+++ app/main.py
@@ -1,26 +1,51 @@
+from __future__ import annotations
+
 import uvicorn
-from fastapi import FastAPI, Query
-from typing import Optional
+from fastapi import FastAPI, Query, Depends
+from typing import Optional, List
 from datetime import date
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
+# class SHotel(BaseModel):
+#     address: str
+#     name: str
+#     stars: int
+
+
+class HotelsSearchArgs:
+    """
+    Аргументы для get запросов
+    """
+    def __int__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: Optional[bool] = None,
+            stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
 @app.get("/hotels")
 def get_hotels(
-        location: str,
-        date_from: date,
-        date_to: date,
-        has_spa: Optional[bool] = None,
-        stars: Optional[int] = Query(None, ge=1, le=5),
+        search_args: HotelsSearchArgs = Depends()
 ):
-    return date_from, date_to
+    return search_args
 
 
 class SBooking(BaseModel):
     """
+    Валидация данных
     Поля, которые пользователь передает в post запросе
+    СХЕМЫ для POST запросов
     """
     room_id: int
     date_from: date
