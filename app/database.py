5fc68e4c8956ccc1d8d3401e2b5b1b8c91df40cd
Valerii <valeri.github@gmail.com>
2023-12-03 15:17:39 +0300

    создать файл .env и comgig.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_USER = "postgres"
-DB_PASS = "postgres"
-DB_NAME = "postgres"
+
 
 # Создаем URL БД
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
