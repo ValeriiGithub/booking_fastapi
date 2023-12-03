6f9719acc9f6ac4a23cc6abe6db17ec9f206e799
Valerii <valeri.github@gmail.com>
2023-12-03 13:50:25 +0300

    настроить файл env модуля alembic

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,17 @@
+import sys
+from os.path import abspath, dirname
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+# говорим Alembic, что он находится в папке app, что бы он понимал как импорты работают
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from app.database import Base
+from app.hotels.models import Hotels
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
