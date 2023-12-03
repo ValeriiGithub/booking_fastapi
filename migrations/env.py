ddc1ded04937031a9ee0a5b07a5da1ab4fce514c
Valerii <valeri.github@gmail.com>
2023-12-03 13:53:36 +0300

    засеттить адрес БД, чтобы alembic понимал с какой БД сравниватьсостояния и создавать миграции

--- migrations/env.py
+++ migrations/env.py
@@ -10,12 +10,14 @@ from alembic import context
 # говорим Alembic, что он находится в папке app, что бы он понимал как импорты работают
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
-from app.database import Base
+from app.database import Base, DATABASE_URL
 from app.hotels.models import Hotels
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
