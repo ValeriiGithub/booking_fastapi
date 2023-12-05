b73dd977ec3247355d033a6d7d15127bbb296c3f
Valerii <valeri.github@gmail.com>
2023-12-05 16:51:19 +0300

    Добавить в импорт созданные модели таблиц БД

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -12,7 +12,9 @@ sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 from app.config import settings
 from app.database import Base
-from app.hotels.models import Hotels
+from app.hotels.models import Hotels, Rooms
+from app.bookings.models import Bookings
+from app.users.models import Users
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
