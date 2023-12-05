fab91068903d78de7ea57880145a4bd13ab68556
Valerii <valeri.github@gmail.com>
2023-12-05 16:01:41 +0300

    Переместить модель Rooms в файл app/hotels/models.py

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -12,3 +12,17 @@ class Hotels(Base):
     services = Column(JSON)
     rooms_quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
+
+
+class Rooms(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    hotel_id = Column(ForeignKey("hotels.id"), nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    price = Column(Integer, nullable=False)
+    services = Column(JSON, nullable=True)
+    quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer)
+    
