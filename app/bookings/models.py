f7315d4fd47c459c561cceffcc206f28a9401aaa
Valerii <valeri.github@gmail.com>
2023-12-06 13:02:28 +0300

    Исправить формула вычисления кол-ва дней

--- app/bookings/models.py
+++ app/bookings/models.py
@@ -12,6 +12,6 @@ class Bookings(Base):
     date_from = Column(Date, nullable=False)
     date_to = Column(Date, nullable=False)
     price = Column(Integer, nullable=False)
-    total_cost = Column(Integer, Computed("(date_from - date_to) * price"))
-    total_days = Column(Integer, Computed("date_from - date_to"))
+    total_cost = Column(Integer, Computed("(date_to - date_from) * price"))
+    total_days = Column(Integer, Computed("date_to - date_from"))
 
