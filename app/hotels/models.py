1c603ffbe811c643d217bfc9526190b41885017f
Valerii <valeri.github@gmail.com>
2023-12-06 12:21:32 +0300

    Создать модель hotels

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -14,14 +14,3 @@ class Hotels(Base):
     image_id = Column(Integer)
 
 
-class Rooms(Base):
-    __tablename__ = "rooms"
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    hotel_id = Column(ForeignKey("hotels.id"), nullable=False)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    price = Column(Integer, nullable=False)
-    services = Column(JSON, nullable=True)
-    quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer)
