fb51ff73da171018a22c81c34d9bb43371178808
Valerii <valeri.github@gmail.com>
2023-12-06 12:23:43 +0300

    Обновить файл env для создания миграции

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -12,7 +12,8 @@ sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 from app.config import settings
 from app.database import Base
-from app.hotels.models import Hotels, Rooms
+from app.hotels.models import Hotels
+from app.hotels.rooms.models import Rooms
 from app.bookings.models import Bookings
 from app.users.models import Users
 
