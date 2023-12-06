a7ca3ea9cd52999a0185cf8f78748c7785cad2a8
Valerii <valeri.github@gmail.com>
2023-12-06 10:31:12 +0300

    Изменить config на лаконичный способ с @property

--- app/config.py
+++ app/config.py
@@ -1,23 +1,58 @@
-# pydantic version 1.10.5
-from pydantic import BaseSettings, root_validator
+from typing import Literal
+
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+    LOG_LEVEL: str
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
 
-    @root_validator
-    def get_database_url(cls, v):
-        v["DATABASE_URL"] = f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
-        return v
+    # Способ ниже оказался чересчур сложным. Обратите внимание на лаконичный способ с @property
+    # @root_validator
+    # def get_database_url(cls, v):
+    #     v["DATABASE_URL"] = f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
+    #     return v
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
+    # @root_validator
+    # def get_test_database_url(cls, v):
+    #     v["TEST_DATABASE_URL"] = f"postgresql+asyncpg://{v['TEST_DB_USER']}:{v['TEST_DB_PASS']}@{v['TEST_DB_HOST']}:{v['TEST_DB_PORT']}/{v['TEST_DB_NAME']}"
+    #     return v
+
+    @property
+    def TEST_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASS: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    SENTRY_DSN: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
     class Config:
-        env_file = "./.env"
+        env_file = ".env"
 
 
 settings = Settings()
-
-
-
