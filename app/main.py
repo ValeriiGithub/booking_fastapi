a2a2da9a2f835d8a3f10a600131d7646e59516d1
Valerii <valeri.github@gmail.com>
2023-12-15 11:50:12 +0300

    импортировать роутер. Прикрепить к приложению роутеры

--- app/main.py
+++ app/main.py
@@ -6,7 +6,11 @@ from typing import Optional, List
 from datetime import date
 from pydantic import BaseModel
 
+from app.bookings.router import router as router_bookings
+
 app = FastAPI()
+
+app.include_router(router_bookings)
 
 
 class HotelsSearchArgs:
