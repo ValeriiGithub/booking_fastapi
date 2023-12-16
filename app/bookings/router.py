bd2b8cabae6e0b415b0046209efd0632d9eba5ab
Valerii <valeri.github@gmail.com>
2023-12-16 18:34:17 +0300

    Создать роутер эндпоинт получить все бронирования

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
+from sqlalchemy import select
+
+from app.bookings.models import Bookings
+from app.database import async_session_maker
 
 router = APIRouter(
     prefix="/bookings",
@@ -8,7 +12,8 @@ router = APIRouter(
 
 @router.get("")
 async def get_bookings():
-    pass
-
-
-
+    with async_session_maker() as session:
+        query = select(Bookings)  # SELECT * FROM bookings;
+        result = await session.execute(query)  # исполни запрос query
+        return result.scalars().all()
+    
