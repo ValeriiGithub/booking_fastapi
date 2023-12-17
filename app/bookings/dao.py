29fe24b9a93709fa324c55fe6c7c5c3680906693
Valerii <valeri.github@gmail.com>
2023-12-17 19:17:03 +0300

    Выделить в отдельную сущность базовый класс

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,17 +1,6 @@
-from app.database import async_session_maker
-
-from sqlalchemy import select
 from app.bookings.models import Bookings
+from app.dao.base import BaseDAO
 
 
-class BookingDAO:
-    """
-    Клас для работы с БД.
-    self - убрано, чтобы каждый раз не создавать объект, а работать сразу с классом
-    """
-    @classmethod
-    async def find_all(cls):
-        async with async_session_maker() as session:
-            query = select(Bookings)  # SELECT * FROM bookings;
-            bookings = await session.execute(query)  # исполни запрос query
-            return bookings.scalars().all()
+class BookingDAO(BaseDAO):
+    model = Bookings
