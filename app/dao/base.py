1593d78fa0a488795b74e4cb37e1b6dfcddf82d3
Valerii <valeri.github@gmail.com>
2023-12-18 12:36:55 +0300

    Добавить метод посика одного объекта или ничего

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,6 +7,13 @@ class BaseDAO:
     model = None
 
     @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)  # SELECT * FROM bookings;
+            result = await session.execute(query)  # исполни запрос query
+            return result.scalar_one_or_none()
+
+    @classmethod
     async def find_all(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**filter_by)  # SELECT * FROM bookings;
