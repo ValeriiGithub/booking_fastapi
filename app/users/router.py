57c47333bc18d570524b905a4ccceed15175df97
Valerii <valeri.github@gmail.com>
2023-12-18 15:38:55 +0300

    Создать роутер для юзера

--- app/users/router.py
+++ app/users/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
+from app.users.schemas import SUserRegister
+
 router = APIRouter(
     prefix="/auth",
     tags=["Auth & Пользователи"],
 )
 
-@router.post("/register"):
-async def register_user()
-    
+@router.post("/register")
+async def register_user(user_data: SUserRegister):
+    exiting_user = 
