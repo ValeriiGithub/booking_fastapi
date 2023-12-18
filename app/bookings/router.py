5ecc409c2e407925868d045cff7cabc51ff5e281
Valerii <valeri.github@gmail.com>
2023-12-18 12:46:38 +0300

    Добавить в метод аннотацию

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.bookings.dao import BookingDAO
+from app.bookings.schemas import SBooking
 
 router = APIRouter(
     prefix="/bookings",
@@ -9,5 +10,5 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings():
+async def get_bookings() -> SBooking:
     return await BookingDAO.find_all()
