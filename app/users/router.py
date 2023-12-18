5b601cc7fbfd69a086e604c0781d6f5078148aa0
Valerii <valeri.github@gmail.com>
2023-12-18 15:42:59 +0300

    Реализовать аутентификацию пользователя

--- app/users/router.py
+++ app/users/router.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
+from app.users.auth import get_password_hash
+from app.users.dao import UsersDAO
 from app.users.schemas import SUserRegister
 
 router = APIRouter(
@@ -9,4 +11,7 @@ router = APIRouter(
 
 @router.post("/register")
 async def register_user(user_data: SUserRegister):
-    exiting_user = 
+    exiting_user = await  UsersDAO.find_one_or_none(email=user_data.email)
+    if exiting_user:
+        raise HTTPException(status_code=500)
+    hashed_password = get_password_hash(user_data.password)
