3a2daa4bb3ab584a0b855fce3abffb0ee521d31d
Valerii <valeri.github@gmail.com>
2023-12-20 17:15:37 +0300

    Обновить ретерн

--- app/users/router.py
+++ app/users/router.py
@@ -26,4 +26,4 @@ async def login_user(response: Response, user_data: SUserAuth):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     access_token = create_access_token({"sub": user.id})
     response.set_cookie("booking_access_token", access_token, httponly=True)
-    return access_token
+    return {"access_token": access_token}
