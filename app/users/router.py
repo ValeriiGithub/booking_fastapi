3b64ad6f619650c31920f42d1165b00ffd91172d
Valerii <valeri.github@gmail.com>
2023-12-20 16:31:36 +0300

    PEP8

--- app/users/router.py
+++ app/users/router.py
@@ -12,7 +12,7 @@ router = APIRouter(
 
 @router.post("/register")
 async def register_user(user_data: SUserRegister):
-    exiting_user = await  UsersDAO.find_one_or_none(email=user_data.email)
+    exiting_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if exiting_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
