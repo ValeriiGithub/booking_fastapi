30d6a6187d9e4379ee03d84a9b27c30d452ce23a
Valerii <valeri.github@gmail.com>
2023-12-20 16:42:35 +0300

    Создать функцию для создания токена (JWT)

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,10 @@
+from datetime import datetime, timedelta
+
+from jose import jwt
+
 from passlib.context import CryptContext
+
+from app.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +15,13 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, settings.ALGORITHM
+    )
+    return encoded_jwt
