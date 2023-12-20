1aabf3d365be4c07d3b912418fe412ff39bbc88f
Valerii <valeri.github@gmail.com>
2023-12-20 17:00:57 +0300

    Исправить БАГ

--- app/dao/base.py
+++ app/dao/base.py
@@ -11,7 +11,7 @@ class BaseDAO:
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(id=model_id)
             result = await session.execute(query)
-            return result.scalar_one_or_none
+            return result.scalar_one_or_none()
 
     @classmethod
     async def find_one_or_none(cls, **filter_by):
