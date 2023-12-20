7041c6a960ced7cf9800bd350158de4a13555ed4
Valerii <valeri.github@gmail.com>
2023-12-20 17:11:51 +0300

    Обезопасить токен

--- app/users/router.py
+++ app/users/router.py
@@ -24,6 +24,6 @@ async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    acces_token = create_access_token({"sub": user.id})
-    response.set_cookie("booking_acces_token", acces_token)
-    return acces_token
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    return access_token
