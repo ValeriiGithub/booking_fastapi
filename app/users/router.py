3e0954d8114055044ea0c65052c05e65da788763
Valerii <valeri.github@gmail.com>
2023-12-26 17:59:54 +0300

    Создать роутер получение текущего юзера

--- app/users/router.py
+++ app/users/router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException, status, Response
+from fastapi import APIRouter, HTTPException, status, Response, Depends
 
 from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 from app.users.schemas import SUserRegister, SUserAuth
 
 router = APIRouter(
@@ -32,3 +34,8 @@ async def login_user(response: Response, user_data: SUserAuth):
 @router.post("/logout")
 async def logout_user(response: Response):
     response.delete_cookie("booking_access_token")
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
