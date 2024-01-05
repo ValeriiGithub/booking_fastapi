b93170615d48bc6834ad56003645ef7342231c73
Valerii <valeri.github@gmail.com>
2024-01-05 16:43:30 +0300

    Add exeption

--- app/users/router.py
+++ app/users/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, status, Response, Depends
 
+from app.exeptions import UserAlreadyExistsException
 from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
 from app.users.dependencies import get_current_user
@@ -16,7 +17,7 @@ router = APIRouter(
 async def register_user(user_data: SUserRegister):
     exiting_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if exiting_user:
-        raise HTTPException(status_code=500)
+        raise UserAlreadyExistsException
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
 
