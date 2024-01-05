96059b426cddf9ceafce98074874ed7555829ca8
Valerii <valeri.github@gmail.com>
2024-01-05 16:57:13 +0300

    Исправлены исключения токен отсутствует

--- app/exeptions.py
+++ app/exeptions.py
@@ -2,11 +2,20 @@ from fastapi import HTTPException, status
 
 UserAlreadyExistsException = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
-    detail="Пользователь уже существует"
+    detail="Пользователь уже существует",
 )
 
 IncorrectEmailOrPasswordException = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Неверная почта или пароль"
+    detail="Неверная почта или пароль",
 )
 
+TokenExpiredException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Токет истек",
+)
+
+TokenAbsentException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Токен отсутствует",
+)
--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -4,13 +4,14 @@ from fastapi import Request, HTTPException, Depends, status
 from jose import jwt, JWTError
 
 from app.config import settings
+from app.exeptions import TokenExpiredException, TokenAbsentException
 from app.users.dao import UsersDAO
 
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenAbsentException
     return token
 
 
@@ -23,7 +24,7 @@ async def get_current_user(token: str = Depends(get_token)):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     expire: str = payload.get("exp")
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenExpiredException
     user_id: str = payload.get("sub")
     if not user_id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
