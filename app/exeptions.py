94fcd4e78cbcfcba00226ef856b3f39db48f3446
Valerii <valeri.github@gmail.com>
2024-01-05 16:45:48 +0300

    Add new exeption

--- app/exeptions.py
+++ app/exeptions.py
@@ -4,3 +4,8 @@ UserAlreadyExistsException = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
     detail="Пользователь уже существует"
 )
+
+IncorrectEMailOrPasswordException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Неверная почта или пароль"
+)
