b7aa24c8ddbe898e34367c609aa9d151bfc15d9f
Valerii <valeri.github@gmail.com>
2024-01-08 16:42:54 +0300

    Реализовать сложный SQL запрос для бронирования дат и комнаты на SQLAlchemy

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,12 +1,72 @@
 # работа с базой данных с использованием реализации паттерна data access object (DAO)
+from datetime import date
+from sqlalchemy import delete, insert, select, func, and_, or_
+# from app.bookings.schemas import SBookingInfo
+from app.dao.base import BaseDAO
+from app.database import async_session_maker, engine
 
 from app.bookings.models import Bookings
-from app.dao.base import BaseDAO
+from app.hotels.rooms.models import Rooms
 
 
 class BookingDAO(BaseDAO):
     model = Bookings
 
-    @classmethod
-    async def add(cls):
-        pass
+@classmethod
+async def add(
+        cls,
+        room_id: int,
+        date_from: date,
+        date_to: date,
+        ):
+    """
+    WITH booked_rooms AS (
+        SELECT * FROM bookings
+        WHERE room_id = 1 AND
+        (date_from >= '2023-05-15' AND date_from <= '2023-06-20' OR
+        date_from <= '2023-05-15' AND date_to > '2023-05-15')
+    )
+    SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms
+    LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
+    WHERE rooms.id = 1
+    GROUP BY rooms.quantity, booked_rooms.room_id
+
+    """
+    async with async_session_maker() as session:
+        booked_rooms = (
+            select(Bookings)
+            .where(
+                and_(
+                    Bookings.room_id == room_id,
+                    or_(
+                        and_(
+                            Bookings.date_from >= date_from,
+                            Bookings.date_from <= date_to,
+                        ),
+                        and_(
+                            Bookings.date_from <= date_from,
+                            Bookings.date_to > date_from,
+                        ),
+                    ),
+                )
+            )
+            .cte("booked_rooms")
+        )
+
+
+        rooms_left = (
+            select(
+                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
+                    "rooms_left"
+                )
+            )
+            .select_from(Rooms)
+            .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
+            .where(Rooms.id == room_id)
+            .group_by(Rooms.quantity, booked_rooms.c.room_id)
+        )
+
+        print(rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
+
+        rooms_left = await session.execute(rooms_left)
+        print(rooms_left.scalar())
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+from datetime import date
 from typing import List
 
 from fastapi import APIRouter, Depends
@@ -21,6 +23,7 @@ async def get_bookings(user: Users = Depends(get_current_user)) -> List[SBooking
 
 @router.post("")
 async def add_booking(
+        room_id: int, date_from: date, date_to: date,
         user: Users = Depends(get_current_user),
 ):
-    await BookingDAO.add()
+    await BookingDAO.add(user.id, room_id, date_from, date_to)
