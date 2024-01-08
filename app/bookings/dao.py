867feee16f379bed1fa40058aae8305a8f97596f
Valerii <valeri.github@gmail.com>
2024-01-08 17:14:44 +0300

    Создать проверку получения комнат

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -59,7 +59,7 @@ async def add(
         # WHERE rooms.id = 1
         # GROUP BY rooms.quantity, booked_rooms.room_id
 
-        rooms_left = (
+        get_rooms_left = (
             select(
                 (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
                     "rooms_left"
@@ -71,7 +71,27 @@ async def add(
             .group_by(Rooms.quantity, booked_rooms.c.room_id)
         )
 
-        print(rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
+        # print(get_rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
 
-        rooms_left = await session.execute(rooms_left)
-        print(rooms_left.scalar())
+        rooms_left = await session.execute(get_rooms_left)
+        rooms_left: int = rooms_left.scalar()
+        # print(rooms_left.scalar())
+
+        if rooms_left > 0:
+            get_price = select(Rooms.price).filter_by(id=room_id)
+            price = await session.execute(get_price)
+            price: int = price.scalar()                     # .scalar() - получение едиснственного значения
+            add_booking = insert(Bookings).values(
+                room_id=room_id,
+                user_id=user_id,
+                date_from=date_from,
+                date_to=date_to,
+                price=price,
+            ).returning(Bookings)
+
+            new_booking = await session.execute(add_booking)
+            await session.commit()                          # Добавить данные в БД
+            return new_booking.scalar()
+        else:
+            return None
+
