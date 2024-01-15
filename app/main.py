d1f798d0374c9b1ddac9893cb01b70c88f31c060
Valerii <valeri.github@gmail.com>
2024-01-15 17:56:35 +0300

    Добавить роутер pages. Добавить TODO

--- app/main.py
+++ app/main.py
@@ -10,10 +10,20 @@ from pydantic import BaseModel
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 
+# TODO: Не реализованы в коде следующие роутеры
+# from app.users.router import router_auth, router_users
+# from app.hotels.router import router as router_hotels
+
+from app.pages.router import router as router_pages
+
 app = FastAPI()
 
+# app.include_router(router_auth)
 app.include_router(router_users)
+# app.include_router(router_hotels)
 app.include_router(router_bookings)
+
+app.include_router(router_pages)
 
 
 class HotelsSearchArgs:
