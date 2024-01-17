860811a30e5372ef08d584da6b7a891bcd7bd4d8
Valerii <valeri.github@gmail.com>
2024-01-17 14:27:43 +0300

    Обновить модель bookings

--- app/bookings/models.py
+++ app/bookings/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, JSON, ForeignKey, Date, Computed
+from sqlalchemy import Column, Computed, Date, ForeignKey, Integer
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -15,3 +16,8 @@ class Bookings(Base):
     total_cost = Column(Integer, Computed("(date_to - date_from) * price"))
     total_days = Column(Integer, Computed("date_to - date_from"))
 
+    user = relationship("Users", back_populates="booking")
+    room = relationship("Rooms", back_populates="booking")
+
+    def __str__(self):
+        return f"Booking #{self.id}"
