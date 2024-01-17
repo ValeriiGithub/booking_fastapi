570ffae59209e6de9d91d705a8cb3a5f20456998
Valerii <valeri.github@gmail.com>
2024-01-17 12:19:41 +0300

    FIX BAG добавить отступ в теле класса

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -12,86 +12,85 @@ from app.hotels.rooms.models import Rooms
 class BookingDAO(BaseDAO):
     model = Bookings
 
-@classmethod
-async def add(
-        cls,
-        user_id: int,
-        room_id: int,
-        date_from: date,
-        date_to: date,
-        ):
-    """
-    WITH booked_rooms AS (
-        SELECT * FROM bookings
-        WHERE room_id = 1 AND
-        (date_from >= '2023-05-15' AND date_from <= '2023-06-20' OR
-        date_from <= '2023-05-15' AND date_to > '2023-05-15')
-    )
-    SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms
-    LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
-    WHERE rooms.id = 1
-    GROUP BY rooms.quantity, booked_rooms.room_id
-
-    """
-    async with async_session_maker() as session:
-        booked_rooms = (
-            select(Bookings)
-            .where(
-                and_(
-                    Bookings.room_id == room_id,
-                    or_(
-                        and_(
-                            Bookings.date_from >= date_from,
-                            Bookings.date_from <= date_to,
-                        ),
-                        and_(
-                            Bookings.date_from <= date_from,
-                            Bookings.date_to > date_from,
-                        ),
-                    ),
-                )
-            )
-            .cte("booked_rooms")
+    @classmethod
+    async def add(
+            cls,
+            user_id: int,
+            room_id: int,
+            date_from: date,
+            date_to: date,
+    ):
+        """
+        WITH booked_rooms AS (
+            SELECT * FROM bookings
+            WHERE room_id = 1 AND
+            (date_from >= '2023-05-15' AND date_from <= '2023-06-20' OR
+            date_from <= '2023-05-15' AND date_to > '2023-05-15')
         )
+        SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms
+        LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
+        WHERE rooms.id = 1
+        GROUP BY rooms.quantity, booked_rooms.room_id
 
-        # SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms
-        # LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
-        # WHERE rooms.id = 1
-        # GROUP BY rooms.quantity, booked_rooms.room_id
-
-        get_rooms_left = (
-            select(
-                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
-                    "rooms_left"
+        """
+        async with async_session_maker() as session:
+            booked_rooms = (
+                select(Bookings)
+                .where(
+                    and_(
+                        Bookings.room_id == room_id,
+                        or_(
+                            and_(
+                                Bookings.date_from >= date_from,
+                                Bookings.date_from <= date_to,
+                            ),
+                            and_(
+                                Bookings.date_from <= date_from,
+                                Bookings.date_to > date_from,
+                            ),
+                        ),
+                    )
                 )
+                .cte("booked_rooms")
             )
-            .select_from(Rooms)
-            .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
-            .where(Rooms.id == room_id)
-            .group_by(Rooms.quantity, booked_rooms.c.room_id)
-        )
 
-        # print(get_rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
+            # SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms
+            # LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
+            # WHERE rooms.id = 1
+            # GROUP BY rooms.quantity, booked_rooms.room_id
 
-        rooms_left = await session.execute(get_rooms_left)
-        rooms_left: int = rooms_left.scalar()
-        # print(rooms_left.scalar())
+            get_rooms_left = (
+                select(
+                    (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
+                        "rooms_left"
+                    )
+                )
+                .select_from(Rooms)
+                .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
+                .where(Rooms.id == room_id)
+                .group_by(Rooms.quantity, booked_rooms.c.room_id)
+            )
 
-        if rooms_left > 0:
-            get_price = select(Rooms.price).filter_by(id=room_id)
-            price = await session.execute(get_price)
-            price: int = price.scalar()                     # .scalar() - получение едиснственного значения
-            add_booking = insert(Bookings).values(
-                room_id=room_id,
-                user_id=user_id,
-                date_from=date_from,
-                date_to=date_to,
-                price=price,
-            ).returning(Bookings)
+            # print(get_rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
 
-            new_booking = await session.execute(add_booking)
-            await session.commit()                          # Добавить данные в БД
-            return new_booking.scalar()
-        else:
-            return None
+            rooms_left = await session.execute(get_rooms_left)
+            rooms_left: int = rooms_left.scalar()
+            # print(rooms_left.scalar())
 
+            if rooms_left > 0:
+                get_price = select(Rooms.price).filter_by(id=room_id)
+                price = await session.execute(get_price)
+                price: int = price.scalar()  # .scalar() - получение едиснственного значения
+                add_booking = insert(Bookings).values(
+                    room_id=room_id,
+                    user_id=user_id,
+                    date_from=date_from,
+                    date_to=date_to,
+                    price=price,
+                ).returning(Bookings)
+
+                new_booking = await session.execute(add_booking)
+                await session.commit()  # Добавить данные в БД
+                return new_booking.scalar()
+            else:
+                return None
