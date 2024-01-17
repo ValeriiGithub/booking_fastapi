dcb16136c0d61383f54f5863dae78065e60af8b2
Valerii <valeri.github@gmail.com>
2024-01-17 13:50:40 +0300

    Создать схемы для hotels для валидации Pydantic

--- app/hotels/schemas.py
+++ app/hotels/schemas.py
@@ -1,11 +1,22 @@
-from pydantic import BaseModel, EmailStr
+from typing import List
+
+from pydantic import BaseModel
 
 
-# class SUserRegister(BaseModel):
-#     email: EmailStr
-#     password: str
-#
-#
-# class SUserAuth(BaseModel):
-#     email: EmailStr
-#     password: str
+class SHotel(BaseModel):
+    id: int
+    name: str
+    location: str
+    services: List[str]
+    rooms_quantity: int
+    image_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SHotelInfo(SHotel):
+    rooms_left: int
+
+    class Config:
+        orm_mode = True
