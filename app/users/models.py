c8b2c4a401bf481e8d585ad68eae5b2a5591c7a9
Valerii <valeri.github@gmail.com>
2024-01-17 14:29:28 +0300

    Обновить модель

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -9,3 +10,8 @@ class Users(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    booking = relationship("Bookings", back_populates="user")
+
+    def __str__(self):
+        return f"Пользователь {self.email}"
