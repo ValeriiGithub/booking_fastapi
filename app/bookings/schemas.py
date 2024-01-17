e402b53b394d0975794d206a333804f7b1e1cfcf
Valerii <valeri.github@gmail.com>
2024-01-17 14:50:55 +0300

    Обновить схему

--- app/bookings/schemas.py
+++ app/bookings/schemas.py
@@ -1,17 +1,35 @@
 from datetime import date
+from typing import Optional, List
 
 from pydantic import BaseModel
 
 
 class SBooking(BaseModel):
-    id = int
-    room_id = int
-    user_id = int
-    date_from = date
-    date_to = date
-    price = int
-    total_cost = int
-    total_days = int
+    id: int
+    room_id: int
+    user_id: int
+    date_from: date
+    date_to: date
+    price: int
+    total_cost: int
+    total_days: int
 
     class Config:
         orm_mode = True
+
+
+class SBookingInfo(SBooking):
+    image_id: int
+    name: str
+    description: Optional[str]
+    services: List[str]
+
+    class Config:
+        orm_mode = True
+
+
+class SNewBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+   
