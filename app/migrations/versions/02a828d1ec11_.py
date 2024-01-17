e9eb9902a89fb11ae19eb6947d62b5b4bed9ad2f
Valerii <valeri.github@gmail.com>
2024-01-17 13:37:41 +0300

    Создать новую миграцию алембик для создания таблиц в PostgreSQL после переустановки системы

---
+++ app/migrations/versions/02a828d1ec11_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 02a828d1ec11
+Revises: 715f9d75cb9f
+Create Date: 2024-01-17 13:17:58.085092
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '02a828d1ec11'
+down_revision: Union[str, None] = '715f9d75cb9f'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
