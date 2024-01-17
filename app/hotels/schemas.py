5e024e9f3e20c0a51ee32aba89a0c00532f409ce
Valerii <valeri.github@gmail.com>
2024-01-17 12:44:06 +0300

    Создать новые файлы в рамках дз № 2

---
+++ app/hotels/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr
+
+
+# class SUserRegister(BaseModel):
+#     email: EmailStr
+#     password: str
+#
+#
+# class SUserAuth(BaseModel):
+#     email: EmailStr
+#     password: str
