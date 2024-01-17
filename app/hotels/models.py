de65bedf0f297a6d950ff4836bfc125a1da01560
Valerii <valeri.github@gmail.com>
2024-01-17 13:48:25 +0300

    Дополнить кодом функцию из образца

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -13,4 +14,7 @@ class Hotels(Base):
     rooms_quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
 
+    rooms = relationship("Rooms", back_populates="hotel")
 
+    def __str__(self):
+        return f"Отель {self.name} {self.location[:30]}"
