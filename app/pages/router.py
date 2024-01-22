63b33b3d51f451f02026600cf7844efb3db118b2
Valerii <valeri.github@gmail.com>
2024-01-22 15:37:17 +0300

    Добавить Depends чтобы вызвать другой роутер hotels

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(
@@ -11,6 +11,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/hotels")
 async def get_hotels_page(
-        request: Request
+        request: Request,
+        hotels=Depends(get_hotels_by_location_and_time)
 ):
     return templates.TemplateResponse(name="hotels.html", context={"request": request})
