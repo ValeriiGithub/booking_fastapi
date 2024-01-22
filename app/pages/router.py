460dd86eace0ed81a00a268f40bed7fecafdde81
Valerii <valeri.github@gmail.com>
2024-01-22 15:43:07 +0300

    Импорт функции

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
+from app.hotels.router import get_hotels_by_location_and_time
+
 router = APIRouter(
     prefix="/pages",
     tags=["Фронтенд"]
