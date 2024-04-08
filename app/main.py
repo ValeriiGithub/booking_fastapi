49e5ead699122fdc11a0046869781cf92bfca99e
Valerii <valeri.github@gmail.com>
2024-04-08 15:21:45 +0300

    Подключение CORS, чтобы запросы к API могли приходить из браузера

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from app.bookings.router import router as router_bookings
@@ -24,4 +25,21 @@ app.include_router(router_pages)
 app.include_router(router_images)
 
 
+# добавление площадок, к-е могут обращаться к нашему API
+# блок кода взят из документации FastAPI
+# Подключение CORS, чтобы запросы к API могли приходить из браузера
+origins = [
+    # 3000 - порт, на котором работает фронтенд на React.js
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
