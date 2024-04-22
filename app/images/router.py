ca54bef51f149fc756ab836684831a17ccdc2bd9
Valerii <valeri.github@gmail.com>
2024-04-22 17:52:16 +0300

    ÐUpdate image router

--- app/images/router.py
+++ app/images/router.py
@@ -1,6 +1,8 @@
 from fastapi import UploadFile, APIRouter
 import shutil
 
+from app.tasks.tasks import process_pic
+
 router = APIRouter(
     prefix="/images",
     tags=["Загрузка картирок"]
@@ -9,5 +11,7 @@ router = APIRouter(
 
 @router.post("/hotels")
 async def add_hotel_image(name: int, file: UploadFile):
-    with open(f"app/static/images/{name}.webp", "wb+") as file_object:
+    im_path = f"app/static/images/{name}.webp"
+    with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+    process_pic.delay(im_path)
