396eb69dc536dfeda2e1f1fa69370826ee44de98
Valerii <valeri.github@gmail.com>
2024-04-29 17:17:42 +0300

    Add SMTP Services

--- app/config.py
+++ app/config.py
@@ -36,10 +36,10 @@ class Settings(BaseSettings):
     # def TEST_DATABASE_URL(self):
     #     return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
     #
-    # SMTP_HOST: str
-    # SMTP_PORT: int
-    # SMTP_USER: str
-    # SMTP_PASS: str
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASS: str
     #
     REDIS_HOST: str
     REDIS_PORT: int
